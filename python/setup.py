350ecb0a730cc5c880808a29de17ab0dfbf80d0c
yuvalavr24 <yuvalavr24@gmail.com>
2024-08-15 14:43:41 +0300

    Fix wheel version

--- python/setup.py
+++ python/setup.py
@@ -2,10 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='tensorflow-graphdef',
-    version='4.25.3-2.13',
+    version='1.0.0',
     packages=find_packages(),
     package_data={
         '': ['LICENSE.txt, NOTICE.txt'],
     },
     include_package_data=True,
+    install_requires=[
+        'protobuf==4.25.3'
+    ]
 )
